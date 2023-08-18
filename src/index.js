a97b7ddc96dac6ed99c65929ff4330a726d4ac5b
Shatrughan Singh Bisen <84929529+Shatrughan-alt@users.noreply.github.com>
2023-08-18 10:56:19 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App"
+
+ReactDOM.render(<App />,document.getElementById("root"));
+
+// const citrus=["Lime ","Lemon","Orange"];
+// const fruits=["Apple","Banana","Coconut", ...citrus];
+// // const fruits=["Apple", ...citrus,"Banana","Coconut"];
+// console.log(fruits);
+
+// const fullName={
+//     fName:"Shatrughan",
+//     lName:"Bisen"
+// }
+// const user={
+//     ...fullName,
+//     id:1,
+//     username:"Jamesbond",
+
+// }
+// console.log(user);
